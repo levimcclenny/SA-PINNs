425faccc6b081ba75cd67b94c8d1d2a2d3c40e1d
Levi <levimcclenny@tamu.edu>
2020-09-15 21:30:05 -0500

    Add versiontest.py and update readme

---
+++ versiontest.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import keras
+import numpy as np
+
+print("tensorflow version:", tf.__version__)
+print("keras version:", keras.__version__)
+print("numpy version:", np.__version__)
